ae1152a5218b3d19a9bca292ffb0ce743c67ce36
Abd-AlRahman-Hoji <abdhoji55@gmail.com>
2026-07-02 23:07:30 +0300

    Add Perfect Numbers solution

---
+++ Problem_03_Perfect_Numbers.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+enum class DivisionStatus {
+    Divides,
+    DoesNotDivide
+};
+
+int readNumber() {
+    int num;
+    do {
+        cout << "Please enter a positive number: ";
+        cin >> num;
+
+        if (num <= 0) {
+            cout << "Invalid input! The number must be greater than 0." << endl;
+        }
+    } while (num <= 0);
+
+    return num;
+}
+
+DivisionStatus checkDivision(int n, int m) {
+    if (n % m == 0) {
+        return DivisionStatus::Divides;
+    }
+    else {
+        return DivisionStatus::DoesNotDivide;
+    }
+}
+
+int calculateSum(int num) {
+    int sum = 0;
+    for (int i = 1; i <= num / 2; i++) {
+        if (checkDivision(num, i) == DivisionStatus::Divides) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+void printResult(int num, int sum) {
+    if (num == sum) {
+        cout << "The number is Perfect" << endl;
+    }
+    else {
+        cout << "The number is Not Perfect" << endl;
+    }
+}
+
+int main() {
+    int number = readNumber();
+    int totalSum = calculateSum(number);
+    printResult(number, totalSum);
+
+    return 0;
+}
